62e85cb0e8748017bf4b783900d545a1d8139aac
Luke De Feo <lukedef@googlemail.com>
2013-08-20 20:24:45 +0100

    sorted out relative resource problem, added nlp tests

--- Test/test_nlp.py
+++ Test/test_nlp.py
@@ -1,15 +1,20 @@
 from NLP_Engine.sentiment_analyser.sentiment_analyser import SentimentAnalyser
 from unittest import TestCase
 import unittest
+from NLP_Engine.sentiment_detector.sentiment_detector import SentimentDetector
 
 __author__ = 'Luke'
 
 
 class TestSentiment(TestCase):
-    def test_sentiment(self):
+    def test_sentiment_analysis(self):
         classifyer = SentimentAnalyser()
         self.assertEqual(classifyer.classify_tweet('omg justin bieber is so amazing'), 'pos')
         self.assertEqual(classifyer.classify_tweet('fml i hate my xbox'), 'neg')
+
+    def test_sentiment_detection(self):
+        detecter = SentimentDetector()
+        print detecter.tweet_contains_sentiment('omg justin bieber is so amazing ')
 
 if __name__ == '__main__':
         unittest.main()
